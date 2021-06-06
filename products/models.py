2ccf8b758f25be3cffc2036b540574bce72fd6de
Artem <uralskiyart@gmail.com>
2021-06-07 02:57:16 +0300

    hw2 is done!

--- products/models.py
+++ products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='имя продукта', max_length=256)
+    image = models.ImageField(upload_to='products_images', blank=True)
+    description = models.TextField(verbose_name='описание продукта', max_length=64, blank=True, null=True)
+    price = models.DecimalField(verbose_name='цена продукта', max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(verbose_name='количество на складе', default=0)
+
+    def __str__(self):
+        return f"{self.name} ({self.category.name})"
+
+
