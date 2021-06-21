fbdbd7ffeefc38bf4c01c46334fd1f3ee75b290e
Artem <uralskiyart@gmail.com>
2021-06-21 17:11:14 +0300

    added UPDATE for admin users

--- admins/forms.py
+++ admins/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from users.forms import UserRegisterForm
+from users.forms import UserRegisterForm, UserProfileForm
 from users.models import User
 
 
@@ -10,3 +10,8 @@ class UserAdminRegisterForm(UserRegisterForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'image', 'first_name', 'last_name', 'password1', 'password2')
+
+
+class UserAdminProfileForm(UserProfileForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4', 'readonly': False}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control py-4', 'readonly': False}))
--- admins/views.py
+++ admins/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 
 from users.models import User
-from admins.forms import UserAdminRegisterForm
+from admins.forms import UserAdminRegisterForm, UserAdminProfileForm
 
 
 def index(request):
@@ -27,7 +27,20 @@ def admin_users_create(request):
 
 
 def admin_users_update(request, id):
-    return render(request, 'admins/admin-users-update-delete.html')
+    selected_user = User.objects.get(id=id)
+    if request.method == 'POST':
+        form = UserAdminProfileForm(data=request.POST, files=request.FILES, instance=selected_user)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserAdminProfileForm(instance=selected_user)
+    context = {
+        'title': 'GeekShop - Админ | Обновление пользователя',
+        'form': form,
+        'selected_user': selected_user,
+    }
+    return render(request, 'admins/admin-users-update-delete.html', context)
 
 
 def admin_users_delete(request, id):
