e7b94d8a3e0e0e9116344e5992e4e7d82a87f6e3
Artem <uralskiyart@gmail.com>
2021-06-22 02:43:28 +0300

    django-admin

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from products.models import ProductCategory, Product
 
+
 admin.site.register(ProductCategory)
-admin.site.register(Product)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'image', 'description', ('price', 'quantity'), 'category')
+    readonly_fields = ('description',)
+    ordering = ('-category',)
+    search_fields = ('name',)
