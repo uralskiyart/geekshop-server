a2eec60fd23d85fb0ffa6bb978903c7eaf81f1d4
Artem <uralskiyart@gmail.com>
2021-06-23 14:02:46 +0300

    hw 7 is done

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+import products.models
 from products.models import ProductCategory, Product
 
 
@@ -11,15 +12,30 @@ class ProductCategoryAdmin(admin.ModelAdmin):
     ordering = ('name',)
     search_fields = ('name',)
 
-
-    def get_queryset(self, request):
-        print(ProductCategory.products_in_category_sum)
-        print(super(ProductCategoryAdmin, self).get_queryset(request).count())
-        return super(ProductCategoryAdmin, self).get_queryset(request)
-
-    def quantity_of_products_in_category(self, obj):
-        return obj.quantity_of_products_in_category
-    quantity_of_products_in_category.short_description = f'Количество товаров в категории '
+    # @staticmethod
+    # def category_id(cat_name):
+    #     return ProductCategory.objects.filter(name=cat_name)
+    #
+    #
+    # @staticmethod
+    # def sum_prod_in_cat(self):
+    #
+    # def get_queryset(self, request):
+    #     # print(Product.objects.filter(category=(super(ProductCategoryAdmin, self).get_queryset(request).filter())))
+    #     # print(super(ProductCategoryAdmin, self).get_queryset(request).count())
+    #     print(request)
+    #     response = super(ProductCategoryAdmin, self).get_queryset(request)
+    #     ips = list(set([obj.name for obj in response]))
+    #     prod_qs = Product.objects.values('quantity', 'category')
+    #     cat_qs = Product.objects.values('id', 'name')
+    #
+    #
+    #     return response
+    #
+    # def quantity_of_products_in_category(self, instance):
+    #     return self.quantity_of_products_in_category.get(instance.name)
+    #
+    # quantity_of_products_in_category.short_description = 'Количество товаров в категории'
 
 
 @admin.register(Product)
