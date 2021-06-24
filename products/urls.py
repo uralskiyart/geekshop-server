f292057b07903d09673f6841598cdfd08542f57f
Artem <uralskiyart@gmail.com>
2021-06-24 17:49:07 +0300

    added paginator to products

--- products/urls.py
+++ products/urls.py
@@ -5,6 +5,7 @@ from products.views import products
 app_name = 'products'
 
 urlpatterns = [
-    path('<int:category_id>/', products, name='product'),
     path('', products, name='index'),
+    path('<int:category_id>/', products, name='product'),
+    path('page/<int:page>/', products, name='page'),
 ]
