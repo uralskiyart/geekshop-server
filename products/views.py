8d155bdf4f8f418aaf69870a81f6831b0160598b
Artem <uralskiyart@gmail.com>
2021-07-13 21:51:27 +0300

    rewrite products controller to CBV

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
 
 from common.views import CommonContextMixin
 
@@ -20,15 +21,32 @@ class IndexView(CommonContextMixin, TemplateView):
 #     return render(request, 'products/index.html', context)
 
 
-def products(request, category_id=None, page=1):
-    context = {'title': 'GeekShop - Каталог', 'categories': ProductCategory.objects.all()}
-    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
-    paginator = Paginator(products, 3)
-    try:
-        products_paginator = paginator.page(page)
-    except PageNotAnInteger:
-        products_paginator = paginator.page(1)
-    except EmptyPage:
-        products_paginator = paginator.page(paginator.num_pages)
-    context['products'] = products_paginator
-    return render(request, 'products/products.html', context)
+class ProductsListView(CommonContextMixin, ListView):
+    model = Product
+    template_name = 'products/products.html'
+    paginate_by = 3
+    title = 'GeekShop - Каталог'
+
+    def get_queryset(self):
+        queryset = super(ProductsListView, self).get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id) if category_id else queryset
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductsListView, self).get_context_data(**kwargs)
+        context['categories'] = ProductCategory.objects.all()
+        return context
+
+
+# def products(request, category_id=None, page=1):
+#     context = {'title': 'GeekShop - Каталог', 'categories': ProductCategory.objects.all()}
+#     products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+#     paginator = Paginator(products, 3)
+#     try:
+#         products_paginator = paginator.page(page)
+#     except PageNotAnInteger:
+#         products_paginator = paginator.page(1)
+#     except EmptyPage:
+#         products_paginator = paginator.page(paginator.num_pages)
+#     context['products'] = products_paginator
+#     return render(request, 'products/products.html', context)
