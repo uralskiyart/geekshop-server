c7ce414181a4eca895274b9d3f25287d53dd6e02
Artem <uralskiyart@gmail.com>
2021-08-02 22:07:14 +0300

    add verify

--- common/views.py
+++ common/views.py
@@ -12,7 +12,6 @@ class CommonContextMixin:
     title = None
 
     def get_context_data(self, **kwargs):
-        print(self.request)
         context = super(CommonContextMixin, self).get_context_data(**kwargs)
         context['title'] = self.title
         return context
